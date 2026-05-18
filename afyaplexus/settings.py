a2eca27afb679cc5db893351b3718270e17ee839
root <root@DESKTOP-FDL90FV.localdomain>
2026-05-18 16:28:29 +0300

    visits

--- afyaplexus/settings.py
+++ afyaplexus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lb&c7dam0m7#*xd1l5k=1rxt1)k-w#t#rr=&!3jnth)32fgqdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
